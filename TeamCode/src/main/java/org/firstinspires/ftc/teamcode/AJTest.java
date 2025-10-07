141d834f2bc32561ef2cbf1e76ff2c1acd26b490
SheaMcCann <154007920+SheaMcCann@users.noreply.github.com>
2025-10-07 14:53:26 -0400

    Working test code for turret shooter!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AJTest.java
@@ -0,0 +1,56 @@
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//
+//
+//public class AJTest extends LinearOpMode{
+//    public DcMotor left;
+//    public DcMotor right;
+//
+//
+//    left = hardwareMap.get()
+//
+//    @Override
+//    public void runOpMode(){
+//
+//    }
+//}
+
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple; //???
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class AJTest extends LinearOpMode {
+    public DcMotor left;
+    public DcMotor right;
+
+    @Override
+    public void runOpMode() {
+
+        left = hardwareMap.get(DcMotor.class, "lm");
+        right = hardwareMap.get(DcMotor.class, "rm");
+
+
+        //left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        waitForStart();
+            while (opModeIsActive()) {
+                left.setPower(1);
+                right.setPower(1);
+            }
+    }
+}
